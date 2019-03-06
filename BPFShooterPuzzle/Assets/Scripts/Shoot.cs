e7344e591c8bf04e2f20d951c456f9d7f4d151e0
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-03-06 14:07:00 +0100

    Updated Unity to latest version -Added new prefab for test push

---
+++ BPFShooterPuzzle/Assets/Scripts/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+	private GameObject prefab;
+
+	[SerializeField] private GameObject muzzle;
+	[SerializeField] private float shootPower = 100;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		prefab = Resources.Load("Projectile") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0))
+		{
+			ShootBullet();
+		}
+    }
+
+	private void ShootBullet()
+	{
+		Transform projectile = Instantiate(prefab, muzzle, Quaternion.identity) as Transform;
+		Rigidbody rb = projectile.GetComponent<Rigidbody>();
+		projectile.transform.parent = null;
+		rb.velocity = muzzle.transform.forward * (shootPower * 10) * Time.deltaTime;
+	}
+}
