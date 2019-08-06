2d554bcb47bb886d4e7ad8a04076f3f27bd9ab6f
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-08-06 16:01:36 +0200

    -Added Unity Example Scenes -Added Gun -Added Gun script -Added Bouncy Projectile -Added Tags.cs -Added Objectives.cs

---
+++ BPFShooterPuzzle/Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	[SerializeField] private float force;
+	[SerializeField] private Transform projectile;
+	public float fireRate = 15f;
+	private float nextTimeToFire = 0f;
+	[SerializeField] private GameObject muzzle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+		{
+			nextTimeToFire = Time.time + 1f / fireRate;
+			Shoot();
+		}
+    }
+
+	private void Shoot()
+	{
+		Transform clone;
+		clone = Instantiate(projectile, muzzle.transform.position, muzzle.transform.rotation);
+
+		// Add force to the cloned object in the object's forward direction
+		clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * force);
+	}
+}
---
+++ BPFShooterPuzzle/Assets/Scripts/Objectives.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+	[SerializeField] private Color col;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == Tags.Projectile)
+		{
+			Debug.Log("DONE");
+			gameObject.GetComponent<Renderer>().material.color = col;
+		}
+	}
+}
